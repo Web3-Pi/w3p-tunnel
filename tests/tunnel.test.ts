e77cb50dbdd595939dd2c918786f8f4a20997339
Seweryn Kras <seweryn.kras@golem.network>
2025-03-25 17:07:51 +0100

    add test to verify that no information leaks between visitors

--- tests/tunnel.test.ts
+++ tests/tunnel.test.ts
@@ -12,6 +12,7 @@ import {
 import type http from "node:http";
 import type { TunnelClient } from "../src/client.ts";
 import assert from "node:assert";
+import net from "node:net";
 
 const SERVER_PORT = 9000;
 const LOCAL_SERVICE_PORT = 8080;
@@ -44,5 +45,50 @@ describe("tunnel", () => {
     const response = await fetch(`http://${SERVER_HOST}:${tunnelPort}/ping`);
     assert.equal(response.status, 200);
     assert.equal(await response.text(), "Hello World\n");
+  });
+
+  it("handles multiple open connections to the same tunnel", async () => {
+    const tunnel = server.tunnels.values().next().value;
+    if (!tunnel) throw new Error("No tunnel found");
+    const tunnelPort = getPortOrThrow(tunnel);
+
+    const createSocket = () => {
+      const socket = new net.Socket();
+      socket.connect(tunnelPort, "localhost");
+      socket.setKeepAlive(true);
+      socket.setTimeout(0);
+      socket.setNoDelay(true);
+      return socket;
+    };
+
+    const makeHttpRequest = async (socket: net.Socket): Promise<Buffer[]> => {
+      const response: Buffer[] = [];
+      socket.on("data", (data) => response.push(data));
+      socket.write("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n");
+      return response;
+    };
+
+    const countHttpResponses = (buffers: Buffer[]): number => {
+      return buffers.reduce(
+        (acc, cur) => acc + (cur.toString().match(/HTTP\/1.1/g) || []).length,
+        0,
+      );
+    };
+
+    const socket1 = createSocket();
+    const socket2 = createSocket();
+
+    try {
+      const response1 = makeHttpRequest(socket1);
+      const response2 = makeHttpRequest(socket2);
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      assert.equal(countHttpResponses(await response1), 1);
+      assert.equal(countHttpResponses(await response2), 1);
+    } finally {
+      socket1.destroy();
+      socket2.destroy();
+    }
   });
 });
